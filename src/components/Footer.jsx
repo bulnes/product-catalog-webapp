20a3a36f45ce7a2a97bee1ac0d64b531d0af15b9
Bruno Bulnes <bulnes.bruno@gmail.com>
2021-12-21 17:45:05 -0300

    style: new css to header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-200">
-      <div className="container mx-auto pt-16 px-10 flex flex-col items-center justify-between md:flex-row md:px-0">
+      <div className="container mx-auto p-10 flex flex-col items-center justify-between md:flex-row md:px-0">
 
-        <h3 className="mb-12 md:mb-20">
+        <h3 className="mb-12 md:mb-0">
           <a href="/" title="Go Home" className="font-bold text-blue-900 text-opacity-90 hover:text-blue-700 text-3xl">
             Product Catalog Webapp
           </a>
         </h3>
 
-        <p className="mb-12 md:mb-20 text-sm">
+        <p className="text-sm">
           &copy; 2021 Bruno Bulnes.  All rights reserved.
         </p>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 bg-white shadow-md mb-5 z-50">
-      <div className="container px-5 md:px-0 pb-3 h-24 flex items-center justify-between mx-auto md:h-auto md:pt-8 md:pb-4 md:items-center">
+      <div className="container p-5 md:py-10 md:px-0 flex items-center justify-between mx-auto md:h-auto md:items-center">
         <h1>
           <a href="/" title="Go Home" className="font-bold text-blue-900 text-opacity-90 hover:text-blue-700 text-3xl">
             Product Catalog Webapp
