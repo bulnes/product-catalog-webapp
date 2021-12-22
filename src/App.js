e2107eb93a61e92f6d08c0d2898e729297e76e10
Bruno Bulnes <bulnes.bruno@gmail.com>
2021-12-21 22:56:41 -0300

    feature: main container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Aside from "./components/Aside";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ProductsList from "./components/ProducstList";
 
 function App() {
+  const [category, setCategory] = React.useState('')
+  const [orderBy, setOrderBy] = React.useState('asc')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='uppercase'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <section className="container mx-auto grid grid-rows-2 grid-cols-1 gap-10 md:grid-rows-1 md:grid-cols-4 my-10">
+        <Aside setCategory={setCategory} orderBy={orderBy} setOrderBy={setOrderBy} />
+        <ProductsList category={category} orderBy={orderBy} />
+      </section>
+
+      <Footer />
+    </>
   );
 }
 
